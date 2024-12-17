a010c2168c50cc5760177aa1bfb90b8e69d8263d
Damien Bafile <bafile.damien@gmail.com>
2024-12-17 18:50:14 +0800

    changed to vec2 for object loc

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
 use macroquad::prelude::*;
+mod object;
+use object::Object;
 
 #[macroquad::main("MyGame")]
 async fn main() {
-    let mut pos = (screen_width() / 2.0, screen_height() / 2.0);
+    let mut player: Object = object::Object {
+        loc: Vec2 {
+            x: screen_width() / 2.0,
+            y: screen_height() / 2.0,
+        },
+        color: GREEN,
+    };
+
     loop {
         clear_background(RED);
 
         let key = get_keys_down();
 
         match key {
-            key if key.contains(&KeyCode::W) => pos.1 -= 1.0,
-            key if key.contains(&KeyCode::S) => pos.1 += 1.0,
-            key if key.contains(&KeyCode::A) => pos.0 -= 1.0,
-            key if key.contains(&KeyCode::D) => pos.0 += 1.0,
+            key if key.contains(&KeyCode::W) => player.loc.y -= 1.0,
+            key if key.contains(&KeyCode::S) => player.loc.y += 1.0,
+            key if key.contains(&KeyCode::A) => player.loc.x -= 1.0,
+            key if key.contains(&KeyCode::D) => player.loc.x += 1.0,
             key if key.contains(&KeyCode::Escape) => break,
             _ => (),
         }
 
-        draw_circle(screen_width() / 2.0 - 15.0, 30.0, 30.0, BLUE);
-        draw_rectangle(pos.0, pos.1, 120.0, 60.0, GREEN);
+        player.draw();
+
+        draw_rectangle(0.0, 1.0, 120.0, 60.0, GREEN);
         draw_text("Hello, world!", 20.0, 20.0, 30.0, DARKGRAY);
 
         next_frame().await
--- src/object.rs
+++ src/object.rs
@@ -1,4 +1,12 @@
-struct Object {
-    x: i32,
-    y: i32,
+use macroquad::prelude::*;
+
+pub struct Object {
+    pub loc: Vec2,
+    pub color: Color,
+}
+
+impl Object {
+    pub fn draw(&self) {
+        draw_circle(self.loc.x as f32, self.loc.y as f32, 15.0, self.color);
+    }
 }
